d8b4c02889f054eb9d7e015898862966815576aa
Caleb L. Power <cpower@axonibyte.com>
2021-11-15 15:49:45 -0600

    implemented Article and User model tests; added copyright / javadocs to new test classes; removed Herobrine

--- src/test/java/com/axonibyte/stentor/EmptyAnswer.java
+++ src/test/java/com/axonibyte/stentor/EmptyAnswer.java
@@ -2,8 +2,13 @@ package com.axonibyte.stentor;
 
 import org.easymock.IAnswer;
 
+/**
+ * Denotes an empty answer to be used when mocking methods with void signatures.
+ * 
+ * @author Caleb L. Power
+ */
 public class EmptyAnswer implements IAnswer<Object> {
-
+  
   @Override public Object answer() throws Throwable {
     return null;
   }
--- src/test/java/com/axonibyte/stentor/net/auth/AuthTokenTest.java
+++ src/test/java/com/axonibyte/stentor/net/auth/AuthTokenTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2021 Axonibyte Innovations, LLC. All rights reserved.
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   
+ *   https://apache.org/licenses/LICENSE-2.0
+ *   
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
 package com.axonibyte.stentor.net.auth;
 
 import java.lang.reflect.Field;
@@ -13,7 +28,7 @@ import com.axonibyte.stentor.persistent.User;
  * 
  * @author Caleb L. Power
  */
-public class AuthTokenTest {
+public final class AuthTokenTest {
   
   /**
    * Tests {@link AuthToken#checkN00bStatus()} to ensure that only the first
